decd087e536646f1e17a5447b39104a1488e5889
Lucas Villanueva <lucasvillanuevagmp@gmail.com>
2023-08-02 17:04:38 -0300

    created helpers and middlewares

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import errorHandler from "./middlewares/errorHandler.js";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.json());
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`GameXpress API listening on port ${PORT}`);
